5efefffc5be7c39472342c98abb7443b097ad8c8
Rovshan Bayramov <rovshan@MacBook-Pro-Rovshan.local>
2026-03-13 10:50:36 +0300

    Fix CORS Allow-Headers for Authorization

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
     const origin = req.headers.origin;
     if (allowed.includes(origin)) res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     if (req.method === "OPTIONS") return res.sendStatus(200);
     next();
 });
